9f94648c5c352ca1d5749d17006aa96fb573fc9d
Bryon Weber <bweber@connectionseducation.com>
2020-02-28 10:29:06 -0500

    Adding support for prefetching fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { observer } from 'mobx-react'
+import { Box } from '@material-ui/core'
 
 import Navigation from './components/navigation'
 import HomePage from './pages/home'
-import { Box } from '@material-ui/core'
 import versionService from './services/version'
 
-@observer
-export default class App extends React.Component {
-    public async componentDidMount(): Promise<void> {
-        await versionService.requestVersion()
-    }
+function App() {
+    useEffect(() => {
+        async function getApiVersion() {
+            await versionService.requestVersion()
+        }
 
-    public render() {
-        return (
-            <Router>
-                <Navigation />
-                <Box>
-                    <Route exact path="/" component={HomePage} />
-                </Box>
-            </Router>
-        )
-    }
+        getApiVersion()
+    }, [])
+
+    return (
+        <Router>
+            <Navigation />
+            <Box>
+                <Route exact path="/" component={HomePage} />
+            </Box>
+        </Router>
+    )
 }
+
+export default observer(App)
