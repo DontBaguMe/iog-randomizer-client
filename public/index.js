4aa229dfec9f3ee10d79b64d6e36b6ef675bbafc
Bryon Weber <bryon.weber@gmail.com>
2020-05-06 18:18:47 -0400

    Adds a webconfig to attempt to rewrite

--- public/index.js
+++ public/index.js
@@ -4,6 +4,6 @@ var options = {
     index: 'index.html',
 }
 server.use('/', express.static('/home/site/wwwroot', options))
-server.get('*', (req, res) => res.sendFile('index.html', { root: Path2D.join(__dirname, '/home/site/wwwroot') }))
+//server.get('*', (req, res) => res.sendFile('index.html', { root: Path2D.join(__dirname, '/home/site/wwwroot') }))
 
 server.listen(process.env.PORT)
