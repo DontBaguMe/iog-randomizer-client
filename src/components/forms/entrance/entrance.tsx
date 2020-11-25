a12a54ef19dac2a77a435b1fba6ff448eb6e2344
Bryon Weber <bryon.weber@gmail.com>
2020-11-25 13:33:34 -0500

    Adds support for entrance shuffle seeds

--- src/components/forms/entrance/entrance.tsx
+++ src/components/forms/entrance/entrance.tsx
@@ -14,6 +14,16 @@ function EntranceShuffleForm() {
         settingsStore.entranceShuffle = value
     }
 
+    const tooltip = <>
+        <span>This setting shuffles where doors and other exits take you.</span>
+        <br />
+        <br />
+        <span>In Coupled mode, doors and exits act normally, i.e. if you backtrack through an exit you'll return to where you entered.</span>
+        <br />
+        <br />
+        <span>In Uncoupled, doors and exits can send you to different places depending on which direction you go through them.</span>
+    </>
+
     return (
         <Grid container spacing={1}>
             <Grid item xs={12}>
@@ -21,7 +31,7 @@ function EntranceShuffleForm() {
                     <InputGroupAddon type="prepend">
                         <InputGroupText>Entrance Shuffle</InputGroupText>
                     </InputGroupAddon>
-                    <Tooltip title="Not Yet Implemented" placement="bottom-start">
+                    <Tooltip title={tooltip} placement="bottom-start">
                         <FormSelect defaultValue={entranceShuffle} onChange={e => onEntranceShuffleChange(e)}>
                             <option value="0">None</option>
                             <option value="1">Coupled</option>
