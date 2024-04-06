494c95225ff8066efdd37e2a5c1570b3865ad5f7
Raeven0 <raeven0@gmail.com>
2024-04-06 12:15:50 -0500

    s/logic/flute

--- src/components/forms/variants/flute.tsx
+++ src/components/forms/variants/flute.tsx
@@ -4,9 +4,9 @@ import { FormSelect, InputGroup, InputGroupAddon, InputGroupText } from 'shards-
 import { settingsStore } from '../../../stores/settings'
 
 function FluteForm() {
-    function onLogicChanged(event: React.ChangeEvent<HTMLSelectElement>) {
+    function onFluteChanged(event: React.ChangeEvent<HTMLSelectElement>) {
         const value = parseInt(event.target.value)
-        settingsStore.logic = value
+        settingsStore.flute = value
     }
 
     return (
@@ -14,7 +14,7 @@ function FluteForm() {
             <InputGroupAddon type="prepend">
                 <InputGroupText>Will's Flute</InputGroupText>
             </InputGroupAddon>
-            <FormSelect defaultValue={settingsStore.flute} onChange={(e) => onLogicChanged(e)}>
+            <FormSelect defaultValue={settingsStore.flute} onChange={(e) => onFluteChanged(e)}>
                 <option value="0">Available from start</option>
                 <option value="1">Shuffled</option>
                 <option value="2">Flute-less</option>
